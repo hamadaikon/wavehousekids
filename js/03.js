3875d3e8c55bfc9de263ccc8e498c9cde218f327
hamadaikon <156398571+hamadaikon@users.noreply.github.com>
2024-03-05 07:02:29 +0900

    weve_house_kids
    ウェーブハウスキッズ公式ホームページです。

---
+++ js/03.js
@@ -0,0 +1,9 @@
+//javaScript
+const loading = document.getElementById('loading');
+
+function animation(){
+  loading.classList.add('loaded');
+}
+
+// window.addEventListener('load', animation);
+window.setTimeout( animation, 2000);
